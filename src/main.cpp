05da27fcc6155e7e7d17db8c3ee41c0833193c82
AmroAlJundi <amr.jundi@gmail.com>
2022-01-22 08:26:36 +0000

    changed int to long long or unsigned int to accommodate the big graphs

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,9 @@
 
 using namespace std;
 
-bool sortedge(const pair<int,int> &a,
-    const pair<int,int> &b) {
+template <typename T>
+bool sortedge(const pair<T,T> &a,
+    const pair<T,T> &b) {
   if(a.first == b.first) {
     return (a.second < b.second);
   } else {
@@ -28,32 +29,32 @@ bool sortedge(const pair<int,int> &a,
 //[ORDERING SYMMETRIC SPARSE MATRICES FOR SMALL PROFILE AND WAVEFRONT]
 
 //b is blockcount
-void testOrder(int *xadj, int* adj, int n, int b, int* order) {
-  int* iperm = new int[n];
-  for(int i = 0; i < n; i++) iperm[order[i]] = i;
+void testOrder(unsigned int *xadj, unsigned int* adj, long long n, long long b, long long* order) {
+  long long* iperm = new long long[n];
+  for(long long i = 0; i < n; i++) iperm[order[i]] = i;
 
-  int max_bw = 0;
+  long long max_bw = 0;
   double mean_bw = 0;
 
-  int bsize = n / b;
+  long long bsize = n / b;
 
-  int** density = new int*[b];
-  for(int i = 0; i < b; i++) {
-    density[i] = new int[b];
-    memset(density[i], 0, sizeof(int) * b); 
+  long long** density = new long long*[b];
+  for(long long i = 0; i < b; i++) {
+    density[i] = new long long[b];
+    memset(density[i], 0, sizeof(long long) * b); 
   }
 
-  for(int i = 0; i < n; i++) {
-    int u = iperm[i];
-    int bu = u / bsize;
+  for(long long i = 0; i < n; i++) {
+    long long u = iperm[i];
+    long long bu = u / bsize;
     if(bu == b) bu--;
-    for(int ptr = xadj[i]; ptr < xadj[i+1]; ptr++) {
-      int v = iperm[adj[ptr]];
-      int bw = abs(u - v);
-      max_bw = max(max_bw, bw);
+    for(long long ptr = xadj[i]; ptr < xadj[i+1]; ptr++) {
+      long long v = iperm[adj[ptr]];
+      long long bw = abs(u - v);
+      max_bw = max<long long>(max_bw, bw);
       mean_bw += bw;
 
-      int bv = v / bsize;
+      long long bv = v / bsize;
       if(bv == b) bv--;
       density[bu][bv]++;      
     }
@@ -64,15 +65,15 @@ void testOrder(int *xadj, int* adj, int n, int b, int* order) {
 
   double para_mean_bw = 0;
   mean_bw = 0;
-  int fblocks = 0;
+  long long fblocks = 0;
   cout << "Printing blocks ----------------------------------------------" << endl;
-  for(int i = 0; i < b; i++) {
-    for(int j = 0; j < b; j++) {
+  for(long long i = 0; i < b; i++) {
+    for(long long j = 0; j < b; j++) {
       cout << std::setprecision(2) << density[i][j] / (xadj[n] + .0f) << "\t";
       if(density[i][j] > 0) {
         fblocks++;
       }
-      int bw = abs(i - j);
+      long long bw = abs(i - j);
       mean_bw += bw * density[i][j];
     }
     cout << endl;
@@ -84,10 +85,10 @@ void testOrder(int *xadj, int* adj, int n, int b, int* order) {
 
 
 #define TPB 128
-void lastvertex(int* xadj, int* adj, int nov, int* distance, int source, int &last, int &dlast) {
-  int jv, cv, noFrontier, noFrontier_prev, fdist = 0;
-  int nextFsize, tEdgesRemain, ptr, eptr;
-  int next = 1;
+void lastvertex(unsigned int* xadj, unsigned int* adj, long long nov, long long* distance, long long source, unsigned int &last, unsigned int &dlast) {
+  long long jv, cv, noFrontier, noFrontier_prev, fdist = 0;
+  long long nextFsize, tEdgesRemain, ptr, eptr;
+  long long next = 1;
   double alpha = 4, beta = 4;
   last = dlast = -1;
   next = 1;
@@ -161,10 +162,10 @@ void lastvertex(int* xadj, int* adj, int nov, int* distance, int source, int &la
   }
 }
 
-int peripheral(int* xadj, int* adj, int n, int start, int* distance, int* Q) {
-  int r = start, r2;
-  int rlevel = -1;
-  int qlevel = 0;
+long long peripheral(unsigned int* xadj, unsigned int* adj, long long n, long long start, long long* distance, long long* Q) {
+  long long r = start, r2;
+  long long rlevel = -1;
+  long long qlevel = 0;
 
   while(rlevel != qlevel) {
     //cout << "Finding peripheral: current dist = " << qlevel << endl;; 
@@ -175,14 +176,14 @@ int peripheral(int* xadj, int* adj, int n, int start, int* distance, int* Q) {
     r = r2;
 #else
 
-    for(int i = 0; i < n; i++) distance[i] = -1;
-    int qrp = 0, qwp = 0;
+    for(long long i = 0; i < n; i++) distance[i] = -1;
+    long long qrp = 0, qwp = 0;
     distance[r] = 0; Q[qwp++] = r;
 
     while(qrp < qwp) {
-      int u = Q[qrp++];
-      for(int ptr = xadj[u]; ptr < xadj[u+1]; ptr++) {
-        int v = adj[ptr];
+      long long u = Q[qrp++];
+      for(long long ptr = xadj[u]; ptr < xadj[u+1]; ptr++) {
+        long long v = adj[ptr];
         if(distance[v] == -1) {
           distance[v] = distance[u] + 1;
           Q[qwp++] = v;
@@ -191,7 +192,7 @@ int peripheral(int* xadj, int* adj, int n, int start, int* distance, int* Q) {
     }
 
     qlevel = 0;
-    for(int i = 0; i < qrp; i++) {
+    for(long long i = 0; i < qrp; i++) {
       if(qlevel < distance[Q[i]]) {
         qlevel = distance[Q[i]];
         r = Q[i];	
@@ -202,13 +203,13 @@ int peripheral(int* xadj, int* adj, int n, int start, int* distance, int* Q) {
   return r;
 }
 
-void rcm_sequential(int *xadj, int* adj, int n, int* Q, int* Qp, int* distance) {
-  int* V = new int[n]; for(int i = 0; i < n; i++) V[i] = 0;
-  priority_queue<pair<int, int> > PQ;
-  int qrp = 0, qwp = 0;
-  int reverse = n-1;
+void rcm_sequential(unsigned int *xadj, unsigned int* adj, long long n, long long* Q, long long* Qp, long long* distance) {
+  long long* V = new long long[n]; for(long long i = 0; i < n; i++) V[i] = 0;
+  priority_queue<pair<long long, long long> > PQ;
+  long long qrp = 0, qwp = 0;
+  long long reverse = n-1;
 
-  for(int i = 0; i < n; i++) {
+  for(long long i = 0; i < n; i++) {
     if(V[i] == 0) {
       if(xadj[i] == xadj[i+1]) {
         Q[reverse--] = i;
@@ -217,13 +218,13 @@ void rcm_sequential(int *xadj, int* adj, int n, int* Q, int* Qp, int* distance) 
       }
 
       // cout << i << endl;
-      int perv = peripheral(xadj, adj, n, i, distance, Qp);      
+      long long perv = peripheral(xadj, adj, n, i, distance, Qp);      
       V[perv] = 1; Q[qwp++] = perv;
 
       while(qrp < qwp) {
-        int u = Q[qrp++];
-        for(int ptr = xadj[u]; ptr < xadj[u+1]; ptr++) {
-          int v = adj[ptr];
+        long long u = Q[qrp++];
+        for(long long ptr = xadj[u]; ptr < xadj[u+1]; ptr++) {
+          long long v = adj[ptr];
           if(V[v] == 0) {
             PQ.push(make_pair(xadj[v + 1] - xadj[v], v));
             V[v] = 1;
@@ -239,16 +240,16 @@ void rcm_sequential(int *xadj, int* adj, int n, int* Q, int* Qp, int* distance) 
   }
 
   //Reverse
-  for(int i = 0; i < n/2; i++) {
-    int t = Q[i];
+  for(long long i = 0; i < n/2; i++) {
+    long long t = Q[i];
     Q[i] = Q[n - i - 1];
     Q[n - i - 1] = t;
   }
 }
 
 int main(int argc, char** argv) {
-  if(argc != 2) {
-    cout << "Use: exec filename (edge list problem) " << endl;
+  if(argc != 3) {
+    cout << "Use: exec filename num_blocks (edge list problem) " << endl;
     return 1;
   }
 
@@ -256,36 +257,36 @@ int main(int argc, char** argv) {
   sprintf(binary_name, "%s.met.bin", argv[1]);
 
   ifstream infile_bin(binary_name, ios::in | ios::binary);
-  int *xadj, m, *adj, *is, n, *tadj;
+  unsigned int *xadj, m, *adj, *is, n, *tadj;
   if(infile_bin.is_open()) {
-    infile_bin.read((char*)(&n), sizeof(int));
-    infile_bin.read((char*)(&m), sizeof(int));
-    xadj = new int[n + 1];
-    infile_bin.read((char*)xadj, sizeof(int) * (n + 1));
+    infile_bin.read((char*)(&n), sizeof(unsigned int));
+    infile_bin.read((char*)(&m), sizeof(unsigned int));
+    xadj = new unsigned int[n + 1];
+    infile_bin.read((char*)xadj, sizeof(unsigned int) * (n + 1));
 
-    adj = new int[m];
-    infile_bin.read((char*)adj, sizeof(int) * m);
+    adj = new unsigned int[m];
+    infile_bin.read((char*)adj, sizeof(unsigned int) * m);
 
-    tadj = new int[m];
-    infile_bin.read((char*)tadj, sizeof(int) * m);
+    tadj = new unsigned int[m];
+    infile_bin.read((char*)tadj, sizeof(unsigned int) * m);
 
-    is = new int[m];
-    infile_bin.read((char*)is, sizeof(int) * m);
+    is = new unsigned int[m];
+    infile_bin.read((char*)is, sizeof(unsigned int) * m);
   } else {
     ifstream infile(argv[1]);
     if(infile.is_open()) {
-      int u, v, edges_read = 0;
+      long long u, v, edges_read = 0;
       n = 0;
 
-      vector< std::pair<int, int> > edges;
+      vector< std::pair<long long, long long> > edges;
       //vertices are 0-based                                                                                                                                                                                                                
       while (infile >> u >> v) {
         if(u != v) {
-          edges.push_back(std::pair<int, int>(u, v));
-          edges.push_back(std::pair<int, int>(v, u));
+          edges.push_back(std::pair<long long, long long>(u, v));
+          edges.push_back(std::pair<long long, long long>(v, u));
 
-          n = max(n, u);
-          n = max(n, v);
+          n = max<long long>(n, u);
+          n = max<long long>(n, v);
 
           edges_read++;
         }
@@ -294,47 +295,47 @@ int main(int argc, char** argv) {
       cout << "No vertices is " << n << endl;
       cout << "No read edges " << edges_read << endl;
 
-      sort(edges.begin(), edges.end(), sortedge);
+      sort(edges.begin(), edges.end(), sortedge<long long>);
       edges.erase( unique( edges.begin(), edges.end() ), edges.end() );
 
       //allocate the memory                                                                                                                                                                                                                 
-      xadj = new int[n + 1];
+      xadj = new unsigned int[n + 1];
 
       m = edges.size();
-      adj = new int[m];
-      tadj = new int[m];
-      is = new int[m];
+      adj = new unsigned int[m];
+      tadj = new unsigned int[m];
+      is = new unsigned int[m];
       cout << "No edges is " << m << endl;
 
       //populate adj and xadj
-      memset(xadj, 0, sizeof(int) * (n + 1));
-      int mt = 0;
-      for(std::pair<int, int>& e : edges) {
+      memset(xadj, 0, sizeof(unsigned int) * (n + 1));
+      long long mt = 0;
+      for(std::pair<long long, long long>& e : edges) {
         xadj[e.first + 1]++;
         is[mt] = e.first;
         adj[mt++] = e.second;
       }
 
-      for(int i = 1; i <= n; i++) {
+      for(long long i = 1; i <= n; i++) {
         xadj[i] += xadj[i-1];
       }
 
-      for(int i = 0; i < m; i++) {
+      for(long long i = 0; i < m; i++) {
         tadj[i] = xadj[adj[i]]++;
       }
-      for(int i = n; i > 0; i--) {
+      for(long long i = n; i > 0; i--) {
         xadj[i] = xadj[i-1];
       }
       xadj[0] = 0;
 
       ofstream outfile_bin(binary_name, ios::out | ios::binary);
       if(outfile_bin.is_open()) {
-        outfile_bin.write((char*)(&n), sizeof(int));
-        outfile_bin.write((char*)(&m), sizeof(int));
-        outfile_bin.write((char*)xadj, sizeof(int) * (n + 1));
-        outfile_bin.write((char*)adj, sizeof(int) * m);
-        outfile_bin.write((char*)tadj, sizeof(int) * m);
-        outfile_bin.write((char*)is, sizeof(int) * m);
+        outfile_bin.write((char*)(&n), sizeof(unsigned int));
+        outfile_bin.write((char*)(&m), sizeof(unsigned int));
+        outfile_bin.write((char*)xadj, sizeof(unsigned int) * (n + 1));
+        outfile_bin.write((char*)adj, sizeof(unsigned int) * m);
+        outfile_bin.write((char*)tadj, sizeof(unsigned int) * m);
+        outfile_bin.write((char*)is, sizeof(unsigned int) * m);
       }
     } else {
       cout << "The file does not exist " << endl;
@@ -342,22 +343,22 @@ int main(int argc, char** argv) {
     }
   }
   cout << "here " << endl;
-  for(int i = 0; i < n; i++) {
-    for(int j = xadj[i]; j < xadj[i + 1]; j++) {
+  for(long long i = 0; i < n; i++) {
+    for(long long j = xadj[i]; j < xadj[i + 1]; j++) {
       if(i != adj[tadj[j]]) {
         cout << "problem: " << i << " " << j << " " << adj[j] << " " << tadj[j] <<  endl;
       }
     }
   }
   cout << "there " << endl;
-  int max_deg = 0, min_deg = n, deg;
-  int* degs = new int[n];
-  memset(degs, 0, sizeof(int) * n);
-  int* ps_degs = new int[n];
-  memset(ps_degs, 0, sizeof(int) * n);
+  long long max_deg = 0, min_deg = n, deg;
+  long long* degs = new long long[n];
+  memset(degs, 0, sizeof(long long) * n);
+  long long* ps_degs = new long long[n];
+  memset(ps_degs, 0, sizeof(long long) * n);
   get_prefixsum_degrees(xadj, n, ps_degs);
 
-  for(int u = 0; u < n; u++) {
+  for(long long u = 0; u < n; u++) {
     deg = (xadj[u + 1] - xadj[u]);
     degs[deg]++;
     if(deg < min_deg) {min_deg = deg;}
@@ -386,16 +387,17 @@ int main(int argc, char** argv) {
   cout << "---------------------------" << endl << endl;
 
 
-  int nthreads = omp_get_max_threads();
+  long long nthreads = omp_get_max_threads();
   cout << "Running with " << nthreads << " threads \n";
 
-  int b = 6;
+  long long b = 6;
+  if (argc > 2) b = atoi(argv[2]);
 
   cout << "---------------------------------------------" << endl;
   cout << "Testing natural order" << endl;
   cout << "---------------------------------------------" << endl;
-  int* order = new int[n];
-  for(int i = 0; i < n; i++) order[i] = i;
+  long long* order = new long long[n];
+  for(long long i = 0; i < n; i++) order[i] = i;
   testOrder(xadj, adj, n, b, order);
 
   cout << endl;
@@ -409,8 +411,8 @@ int main(int argc, char** argv) {
   cout << "---------------------------------------------" << endl;
   cout << "Testing RCM order" << endl;
   cout << "---------------------------------------------" << endl;
-  int* Qb = new int[n];  
-  int* distance = new int[n];
+  long long* Qb = new long long[n];  
+  long long* distance = new long long[n];
   double startt = omp_get_wtime();
   rcm_sequential(xadj, adj, n, order, distance, Qb);
   double endt = omp_get_wtime();
